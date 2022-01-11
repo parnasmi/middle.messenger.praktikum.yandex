d70af81baeba1170f5992f237a637b3f45bd8bbb
Ilhom Maksadkulov <parnasmi@gmail.com>
2022-01-11 08:20:28 +0500

    init: add parcel and some configs

---
+++ src/modules/sum.js
@@ -0,0 +1,6 @@
+export function sum(...args) {
+    if (args.length === 0) {
+        throw Error('sum required at least 1 argument')
+    }
+    return args.reduce((result, current) => result + current, 0);
+}
