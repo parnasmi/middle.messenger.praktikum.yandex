5784bc0e7cf394a2f2fae7ce6dd91bb79f36c03b
Ilhom Max <parnasmi@gmail.com>
2022-02-09 06:29:07 +0500

    Update src/pages/404/404.ts
    Co-authored-by: DmitryKhitrin <32079217+DmitryKhitrin@users.noreply.github.com>

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -2,6 +2,7 @@ import {Block} from "../../shared/utils";
 import tmpl from './404.tmpl.hbs'
 import '../../scss/styles.scss'
 import '../../scss/pages/service-page.scss'
+
 export class NotFound extends Block {
 	constructor() {
 	document.body.className = 'h-screen not-auth w-screen';
